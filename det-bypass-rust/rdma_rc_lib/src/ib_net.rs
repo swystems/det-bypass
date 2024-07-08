a057e791e1a1508fd8459027594c100e00081f4c
JacobSalvi <jacobsalvi610@protonmail.ch>
2024-07-08 11:55:32 +0200

    Add bindings for rdma.

--- det-bypass-rust/rdma_rc_lib/src/ib_net.rs
+++ det-bypass-rust/rdma_rc_lib/src/ib_net.rs
@@ -1,18 +1,63 @@
-use ibverbs::{Device, DeviceList};
+use std::io::Error;
+use std::io::ErrorKind;
+
+use rand::rngs::StdRng;
+use rand::Rng;
+use rand::SeedableRng;
+use rdma::device;
+use rdma::bindings;
 
 
 
-
-pub fn ib_device_find_by_name<'a>(device_list: &'a DeviceList, name: &str) -> Option<Device<'a>> {
-    let mut dev = None;
-    for d in device_list{
-        let dev_name = d.name();
-        match dev_name {
-            Some(d_name) => if name == d_name.to_str().unwrap() {
-                dev = Some(Device::from(d));
-            } ,
-            None => ()
-        };
+pub fn ib_device_find_by_name<'a>(device_list: &'a device::DeviceList, name: &str) -> Result<Option<&'a device::Device>, std::io::Error>{
+    let mut dev: Option<&rdma::device::Device> = None;
+    for device in device_list.as_slice() {
+        if name == device.name(){
+            dev = Some(device.clone());
+        }
     }
-    return dev;
+    Ok(dev)
 }
+
+
+struct IbNodeInfo {
+    lid: u16,
+    qpn: u32,
+    psn: u32,
+    gid: bindings::ibv_gid
+}
+
+
+pub fn ib_get_local_info(context: *mut bindings::ibv_context, ib_port: u8, gidx: i32, qp: bindings::ibv_qp, out: &mut IbNodeInfo, seed: u64) -> Result<(), std::io::Error>{
+    let port_info: *mut bindings::ibv_port_attr = std::ptr::null_mut();
+    unsafe{
+        if bindings::ibv_query_port(context, ib_port, port_info) != 0{ 
+            eprintln!("Couldn't get port info");
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, "writing to bucket failed"));
+        }
+    
+        out.lid = (*(port_info)).lid;
+        if (*port_info).link_layer as u32 != bindings::IBV_LINK_LAYER_ETHERNET && out.lid == 0{
+            eprintln!("Couldn't get local LID");
+            return Err(Error::new(ErrorKind::Other, "Couldn't get port info"));
+
+        }
+        if bindings::ibv_query_gid(context, ib_port, gidx, &mut out.gid) != 0{
+            eprintln!("Couldn't get local GID");
+            return Err(Error::new(ErrorKind::Other, "Couldn't get local GID"));
+        }
+        out.qpn = qp.qp_num;
+        let mut r = StdRng::seed_from_u64(seed);
+        out.psn = r.gen::<u32>() & 0xffffff; 
+    }
+    Ok(())
+}
+
+
+pub fn ib_print_node_info(info: &IbNodeInfo){
+    unsafe {
+        let res = std::net::Ipv6Addr::from(info.gid.raw);
+        println!("Address: LID 0x{:04x}, QPN 0x{:06x}, PSN 0x{:06x}, GID {}", info.lid, info.qpn, info.psn, res);
+    }
+}
+
--- det-bypass-rust/rdma_rc_lib/src/lib.rs
+++ det-bypass-rust/rdma_rc_lib/src/lib.rs
@@ -1,10 +1,8 @@
 use std::{process, time::{SystemTime, UNIX_EPOCH}};
 
-use common::persistence_agent::PingPongPayload;
+use std::io::Error;
 use ib_net::ib_device_find_by_name;
-use rand::{rngs::StdRng, SeedableRng};
-use ibverbs::DeviceList;
-use std::sync::atomic::AtomicU8;
+use rdma::device;
 
 mod ib_net;
 
@@ -21,54 +19,54 @@ fn initialize_random_number_generator() -> u64 {
 
 
 
-pub fn run_client(ib_devname: &str, port_gid_idx: u32, iters: u64, interval: u64, server_ip: &str, pf: &str) {
+pub fn run_client(ib_devname: &str, port_gid_idx: u32, iters: u64, interval: u64, server_ip: &str, pf: &str) -> Result<(), Error> {
     let persistence_flag = common::persistence_agent::pers_measurament_to_flag(pf);
     let _seed = initialize_random_number_generator();
 
-    let device_list = ibverbs::devices(); 
-    let device_list: DeviceList = match device_list {
-        Ok(dl) => dl,
-        Err(e) => panic!("{e}") 
+    let device_list = rdma::device::DeviceList::available();
+    let device_list: rdma::device::DeviceList = match device_list {
+        Err(e) => return Err(Error::new(std::io::ErrorKind::Other, "writing to bucket failed")),
+        Ok(dl) => dl
     };
     
     let device = ib_device_find_by_name(&device_list, ib_devname);
     match device {
-        None => panic!("IB device not found"),
-        _ => ()
+        Err(e) => Err(e),
+        Ok(dev) => Ok(())
     }
 }
 
 union PingPongUnion{
     buff: u8,
-    payload: PingPongPayload
+    //payload: PingPongPayload
 }
 
 
 
-struct PingpongContext {
-    pending: AtomicU8,// WID of the pending WR
+//struct PingpongContext {
+//    pending: AtomicU8,// WID of the pending WR
+//
+//    send_flags: u32,
+//
+//    recv: PingPongUnion,
+//    send: PingPongUnion,
+//
+//    struct ibv_context *context;
+//
+//    struct ibv_pd *pd;
+//    uint64_t completion_timestamp_mask;
+//
+//    struct ibv_mr *recv_mr;
+//    struct ibv_mr *send_mr;
+//
+//    struct ibv_cq_ex *cq;
+//
+//    struct ibv_qp *qp;
+//    struct ibv_qp_ex *qpx;
+//};
+//
 
-    send_flags: u32,
-
-    recv: PingPongUnion,
-    send: PingPongUnion,
-
-    struct ibv_context *context;
-
-    struct ibv_pd *pd;
-    uint64_t completion_timestamp_mask;
-
-    struct ibv_mr *recv_mr;
-    struct ibv_mr *send_mr;
-
-    struct ibv_cq_ex *cq;
-
-    struct ibv_qp *qp;
-    struct ibv_qp_ex *qpx;
-};
-
-
-fn pp_init_context(device: &Device){
+fn pp_init_context(device: & device::Device){
     
 
 }
