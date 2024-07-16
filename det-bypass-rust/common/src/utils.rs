3546c8cd80a647c3891b3efce80c25547211026d
JacobSalvi <jacobsalvi610@protonmail.ch>
2024-07-16 15:40:55 +0200

    Improved busy waiting.

--- det-bypass-rust/common/src/utils.rs
+++ det-bypass-rust/common/src/utils.rs
@@ -4,24 +4,31 @@ pub fn get_time_ns() -> u64 {
     let duration = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .expect("SystemTime before UNIX EPOCH!");
-
-    duration.as_secs() * 1_000_000_000 + duration.subsec_nanos() as u64
+    duration.as_nanos() as u64
 }
 
 
-pub fn pp_sleep(ns: u64){
+pub fn pp_sleep(mut ns: u64){
     if ns==0{
         return;
     }
-    // 1 ms
-    if ns < 1_000_000 {
-        let start = get_time_ns();
-        while get_time_ns() - start < ns {
-            std::hint::spin_loop();
-        } 
-    } else {
-        std::thread::sleep(std::time::Duration::from_nanos(ns));
+    let start = std::time::Instant::now();
+
+    if ns > 1_000_000{
+        let sleep_duration = ns - 1_000_000;
+        std::thread::sleep(std::time::Duration::from_nanos(sleep_duration));
+        let now = std::time::Instant::now();
+        // One might expect the diff to always be 1_000_000 nanoseconds,
+        // but given that thread::sleep might sleep for more than the requested time
+        // it might not always be the case.
+        let diff =  now.duration_since(start);
+        ns -= diff.as_nanos() as u64;
     }
+    let new_start = get_time_ns();
+    while get_time_ns() - new_start< ns {
+        std::hint::spin_loop();
+    }
+        
 }
 
 pub fn new_error<T>(msg: &str) -> Result<T, Error>{
