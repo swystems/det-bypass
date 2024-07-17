d94e715fe188fd72826d441227076893ba319696
JacobSalvi <jacobsalvi610@protonmail.ch>
2024-07-17 12:06:31 +0200

    Change the size passed to 'ibv_reg_mr'.

--- det-bypass-rust/rdma_rc_lib/src/pingpong_context.rs
+++ det-bypass-rust/rdma_rc_lib/src/pingpong_context.rs
@@ -1,10 +1,6 @@
 use std::{fmt, io::{Error, ErrorKind} };
 use common::consts;
-use rdma::{bindings, cq, device::{self, Mtu}, mr::AccessFlags, pd::ProtectionDomain, poll_cq_attr::PollCQAttr, qp::{self, ModifyOptions, QueuePairState}};
-
-
-const RECEIVE_DEPTH: usize = 500;
-const IB_PORT: u8 = 1;
+use rdma::{cq, device::{self, Mtu}, mr::AccessFlags, pd::ProtectionDomain, poll_cq_attr::PollCQAttr, qp};
 
 
 #[derive(Debug)]
@@ -47,67 +43,6 @@ pub struct PingPongContext {
 
 
 impl PingPongContext{
-    pub fn new (device: & rdma::device::Device, recv_buf: *mut u8, send_buf: *mut u8, recv_size: usize, send_size: usize) -> Result<Self, std::io::Error>{
-        let context = match device.open(){
-            Ok(ctx) => ctx,
-            Err(e) => return Err(e)
-        };
-        let pd = match ProtectionDomain::alloc(&context){
-            Ok(prot_dom) => prot_dom,
-            Err(e) => return Err(e)
-        };
-
-         
-        let recv_mr = unsafe {
-            match rdma::mr::MemoryRegion::register(&pd, recv_buf, consts::PACKET_SIZE, AccessFlags::LOCAL_WRITE, ()){
-                Ok(mr) => mr,
-                Err(e) => return Err(e)
-            }
-        }; 
-        let send_mr = unsafe {
-            match rdma::mr::MemoryRegion::register(&pd, send_buf, consts::PACKET_SIZE, AccessFlags::LOCAL_WRITE, ()){
-                Ok(mr) => mr,
-                Err(e) => return Err(e)
-            }
-        };
-
-        let mut options = rdma::cq::CompletionQueue::options();
-        options.cqe(RECEIVE_DEPTH+1);
-        options.wc_flags(bindings::IBV_WC_EX_WITH_COMPLETION_TIMESTAMP as u64);
-
-        let cq = match rdma::cq::CompletionQueue::create(&context, options){ 
-            Ok(cq) => cq,
-            Err(e) => return Err(e)
-        };
-        
-        let mut qp_options: rdma::qp::QueuePairOptions = rdma::qp::QueuePair::options();
-        qp_options.send_cq(&cq);
-        qp_options.recv_cq(&cq);
-        qp_options.cap(rdma::qp::QueuePairCapacity{max_send_wr: 1, max_recv_wr: RECEIVE_DEPTH as u32, max_send_sge: 1, max_recv_sge: 1, max_inline_data: 0});
-        qp_options.qp_type(rdma::qp::QueuePairType::RC);
-        qp_options.comp_mask(bindings::IBV_QP_INIT_ATTR_PD | bindings::IBV_QP_INIT_ATTR_SEND_OPS_FLAGS);
-        qp_options.pd(&pd);
-        qp_options.send_ops_flags(bindings::IBV_QP_EX_WITH_SEND);
-        let qp = match rdma::qp::QueuePair::create(&context, qp_options){
-            Ok(qp) => qp,
-            Err(_) => return Err(Error::new(ErrorKind::Other, "Couldn't create qp"))
-        };
-        
-        
-        let mut modify_options: ModifyOptions = ModifyOptions::default();
-        modify_options.qp_state(QueuePairState::Initialize);
-        modify_options.pkey_index(0);
-        modify_options.port_num(IB_PORT);
-        modify_options.qp_access_flags(AccessFlags::empty());
-        
-        match qp.modify(modify_options){
-            Ok(_) => (),
-            Err(_) => return Err(Error::new(ErrorKind::Other, "Failed to modify QP to INIT"))
-        };
-
-        Ok(PingPongContext{context, pd, recv_mr, send_mr, cq, qp})
-    }
-
     pub fn modify_qp(&mut self, options: qp::ModifyOptions) -> Result<(), std::io::Error> {
         self.qp.modify(options)
     }
@@ -208,13 +143,13 @@ impl <'a>PPContextBuilder<'a>{
 
          
         let recv_mr = unsafe {
-            match rdma::mr::MemoryRegion::register(&pd, self.recv_buf.unwrap(), self.recv_size.unwrap(), AccessFlags::LOCAL_WRITE, ()){
+            match rdma::mr::MemoryRegion::register(&pd, self.recv_buf.unwrap(), consts::PACKET_SIZE ,AccessFlags::LOCAL_WRITE, ()){
                 Ok(mr) => mr,
                 Err(e) => return Err(e)
             }
         }; 
         let send_mr = unsafe {
-            match rdma::mr::MemoryRegion::register(&pd, self.send_buf.unwrap(), self.send_size.unwrap(), AccessFlags::LOCAL_WRITE, ()){
+            match rdma::mr::MemoryRegion::register(&pd, self.send_buf.unwrap(), consts::PACKET_SIZE, AccessFlags::LOCAL_WRITE, ()){
                 Ok(mr) => mr,
                 Err(e) => return Err(e)
             }
