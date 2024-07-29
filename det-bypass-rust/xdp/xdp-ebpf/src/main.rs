6a268353f852d017a52f2b9343ad899483bc49db
JacobSalvi <jacobsalvi610@protonmail.ch>
2024-07-29 09:58:19 +0200

    Experiment with xdp

---
+++ det-bypass-rust/xdp/xdp-ebpf/src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+use aya_log_ebpf::info;
+
+#[xdp]
+pub fn xdp(ctx: XdpContext) -> u32 {
+    match try_xdp(ctx) {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
+
+fn try_xdp(ctx: XdpContext) -> Result<u32, u32> {
+    info!(&ctx, "received a packet");
+    Ok(xdp_action::XDP_PASS)
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
